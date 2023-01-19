8fa8b8580b7ba4839007a95f55ce15c776b2a003
pohjie <0xArlet@gmail.com>
2023-01-19 13:00:54 +0800

    Fill in selected dot with color

--- image-slider/image-slider.js
+++ image-slider/image-slider.js
@@ -1,36 +1,50 @@
 let currX = 0;
 
-const wideImageDiv = document.getElementById('wide-image');
+const wideImageDiv = document.getElementById("wide-image");
 
 const addTranslationToStyle = (currX) => {
   wideImageDiv.style.transform = `translateX(${currX}vw)`;
-  wideImageDiv.style.transition = 'transform 330ms ease-in-out';
-}
+  wideImageDiv.style.transition = "transform 330ms ease-in-out";
+};
 const next = () => {
   currX = (currX - 20) % 100;
   addTranslationToStyle(currX);
-}
+};
 
 const prev = () => {
   currX = currX === 0 ? -80 : (currX + 20) % 100;
   addTranslationToStyle(currX);
-}
+};
 
-const nextBtn = document.getElementById('next-btn');
-nextBtn.addEventListener('click', next);
+const nextBtn = document.getElementById("next-btn");
+nextBtn.addEventListener("click", next);
 
-const prevBtn = document.getElementById('prev-btn');
-prevBtn.addEventListener('click', prev);
+const prevBtn = document.getElementById("prev-btn");
+prevBtn.addEventListener("click", prev);
+
+const smallDotsArr = document.querySelectorAll(".small-dot");
 
 const moveToImg = (event) => {
   currX = event.srcElement.index * -20;
   addTranslationToStyle(currX);
+};
+
+const updateFilledDot = (event) => {
+  for (let i = 0; i < smallDotsArr.length; i++) {
+    if (i !== event.srcElement.index) {
+      smallDotsArr[i].classList.remove('selected-dot');
+    } else {
+      smallDotsArr[i].classList.add('selected-dot');
+    }
+  }
 }
 
-const smallDotsArr = document.querySelectorAll('.small-dot');
 let i = 0;
 smallDotsArr.forEach((smallDot) => {
   smallDot.index = i;
   i++;
-  smallDot.addEventListener('click', (event) => moveToImg(event));
+  smallDot.addEventListener("click", (event) => {
+    moveToImg(event);
+    updateFilledDot(event);
+  });
 });
