d375943f7bec169e9ed40cce7fc4d486ef2b9789
pohjie <0xArlet@gmail.com>
2023-01-19 13:18:55 +0800

    Align next and prev button with nav circles

--- image-slider/image-slider.js
+++ image-slider/image-slider.js
@@ -1,11 +1,14 @@
 let currX = 0;
 
+const xIncrement = -20;
+
 const wideImageDiv = document.getElementById("wide-image");
 
 const addTranslationToStyle = (currX) => {
   wideImageDiv.style.transform = `translateX(${currX}vw)`;
   wideImageDiv.style.transition = "transform 330ms ease-in-out";
 };
+
 const next = () => {
   currX = (currX - 20) % 100;
   addTranslationToStyle(currX);
@@ -17,10 +20,18 @@ const prev = () => {
 };
 
 const nextBtn = document.getElementById("next-btn");
-nextBtn.addEventListener("click", next);
+nextBtn.addEventListener("click", () => {
+  next();
+  updateFilledDot(currX / xIncrement);
+  resetInterval();
+});
 
 const prevBtn = document.getElementById("prev-btn");
-prevBtn.addEventListener("click", prev);
+prevBtn.addEventListener("click", () => {
+  prev();
+  updateFilledDot(currX / xIncrement);
+  resetInterval();
+});
 
 const smallDotsArr = document.querySelectorAll(".small-dot");
 
@@ -29,15 +40,15 @@ const moveToImg = (event) => {
   addTranslationToStyle(currX);
 };
 
-const updateFilledDot = (event) => {
+const updateFilledDot = (idx) => {
   for (let i = 0; i < smallDotsArr.length; i++) {
-    if (i !== event.srcElement.index) {
-      smallDotsArr[i].classList.remove('selected-dot');
+    if (i !== idx) {
+      smallDotsArr[i].classList.remove("selected-dot");
     } else {
-      smallDotsArr[i].classList.add('selected-dot');
+      smallDotsArr[i].classList.add("selected-dot");
     }
   }
-}
+};
 
 let i = 0;
 smallDotsArr.forEach((smallDot) => {
@@ -45,6 +56,13 @@ smallDotsArr.forEach((smallDot) => {
   i++;
   smallDot.addEventListener("click", (event) => {
     moveToImg(event);
-    updateFilledDot(event);
+    updateFilledDot(event.srcElement.index);
+    resetInterval();
   });
 });
+
+let nextImgIntervalId = setInterval(next, 5000);
+const resetInterval = () => {
+  clearInterval(nextImgIntervalId);
+  nextImgIntervalId = setInterval(next, 5000);
+};
